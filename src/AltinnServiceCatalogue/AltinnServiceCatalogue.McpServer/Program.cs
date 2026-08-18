70ecea15190f80ccf9dbf84773c19d60c69b58e9
Tømmerås-Larsen <rune.tommeras-larsen@digdir.no>
2026-08-18 08:11:49 +0200

    chore: upgrade MCP SDK to 2.2

--- src/AltinnServiceCatalogue/AltinnServiceCatalogue.McpServer/Program.cs
+++ src/AltinnServiceCatalogue/AltinnServiceCatalogue.McpServer/Program.cs
@@ -1,8 +1,14 @@
 using AltinnServiceCatalogue.McpServer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Logging.AddConsole(options =>
+{
+    options.LogToStandardErrorThreshold = LogLevel.Trace;
+});
 
 builder.Services.AddHttpClient("Altinn", client =>
 {
